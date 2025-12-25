f79af8ae83252cf7f51eeb7fc8a3004c18660ff6
Jiayan Zhou <43587078+nicenzhou@users.noreply.github.com>
2025-12-24 20:46:54 -0800

    Create __init__.py

---
+++ edge_gwas/__init__.py
@@ -0,0 +1,26 @@
+"""
+EDGE-GWAS: Encoding Deviation Genotypic Effects GWAS
+
+A Python package for performing EDGE GWAS analysis with flexible encoding
+to identify nonadditive SNP effects.
+
+References:
+    Zhou, J. et al. Flexibly encoded GWAS identifies novel nonadditive SNPs 
+    in individuals of African and European ancestry. medRxiv 2023.06.01.23290857
+    
+    Hall, M. A. et al. Novel EDGE encoding method enhances ability to identify 
+    genetic interactions. PLoS Genetics 17, e1009534 (2021).
+"""
+
+__version__ = "1.0.0"
+__author__ = "Jiayan Zhou"
+__email__ = "jyzhou@stanford.edu"
+
+from .core import EDGEAnalysis
+from .utils import load_plink_data, prepare_phenotype_data
+
+__all__ = [
+    "EDGEAnalysis",
+    "load_plink_data",
+    "prepare_phenotype_data",
+]
